0761812193a6071e3b5c5df168eb88e3350cb4b3
Jorge <jorgereyeslira02@gmail.com>
2025-08-16 22:06:34 -0500

    Primer Componente

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { FirstStepsApp } from './FirstStepsApp'
 
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hola Mundo</h1>
+    
+    <FirstStepsApp/>
+    
   </React.StrictMode>,
 )
