758170c0b46176953c037c02f766d200484a8217
Jorge <jorgereyeslira02@gmail.com>
2025-08-17 21:04:40 -0500

    Eventos de los elementos

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -6,6 +6,11 @@ interface Props {
 }
 
 export const ItemCounter = ({ name, quantity }: Props) => {
+
+    const handleClick = () => {
+        console.log(`Click ${name}`)
+    }
+
     return (
         <section style={{
             display: 'flex',
@@ -17,7 +22,7 @@ export const ItemCounter = ({ name, quantity }: Props) => {
             <span style={{
                 width: 150,
             }}> {name}</span>
-            <button>+1</button>
+            <button onClick={handleClick}>+1</button>
             <span>{quantity}</span>
             <button>-1</button>
         </section>
