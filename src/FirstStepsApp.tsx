bf3115d195380a34e68a6e9633dcae0674ff252b
Jorge <jorgereyeslira02@gmail.com>
2025-08-17 20:55:03 -0500

    Mostrar listados de Elementos

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,12 +1,32 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+
+interface ItemInCart {
+    productName: string;
+    quantity: number
+}
+
+const itemsInCart: ItemInCart[] = [
+    { productName: 'Play Station 5', quantity: 5 },
+    { productName: 'Xbox Series x', quantity: 2 },
+    { productName: 'Pc Gamer', quantity: 3 },
+]
+
+
 export function FirstStepsApp() {
 
     return (
         <>
-            <ItemCounter name='Play Station 5' quantity={4} />
+            <h1>Carrito de Compras</h1>
+
+            {
+                itemsInCart.map(({ productName, quantity }) => (
+                    <ItemCounter key={productName} name={productName} quantity={quantity} />
+                ))
+            }
+            {/* <ItemCounter name='Play Station 5' quantity={4} />
             <ItemCounter name='Xbox Series x' quantity={1} />
-            <ItemCounter name='Pc Gamer' quantity={2} />
+            <ItemCounter name='Pc Gamer' quantity={2} /> */}
         </>
 
     )
--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { MyAwesomeApp } from './MyAwesomeApp'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
 
-    <h1>Carrito de Compras</h1>
+
     <FirstStepsApp />
 
     {/* <MyAwesomeApp /> */}
